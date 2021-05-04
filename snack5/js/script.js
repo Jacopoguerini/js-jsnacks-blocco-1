b005924e7171b712e4de6312e9245e34c30c8143
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-04 16:19:17 +0200

    Snack 5 completato

--- snack5/js/script.js
+++ snack5/js/script.js
@@ -1,23 +1,26 @@
 // Snack #5:
 
 // Crea un array vuoto.
-var arrayVuoto = [];
+var numeri = [];
 
 // Chiedi per 6 volte all’utente di inserire un numero
 for (var i = 0; i < 6; i++) {
     var numeroUtente = parseInt(prompt("Inserisci un numero qui sotto:"));
     console.log(numeroUtente);
+
+    if (seDispari(numeroUtente)) {
+        numeri.push(numeroUtente);
+    }
 }
 
 // Funzione per verificare se il numero inserito è dispari se è dispari e se lo è, inseriscilo nell'array
-function numeroDispari(num) {
+function seDispari(num) {
     var controllo = false;
     if (num % 2 != 0) {
-        controllo = true
+        controllo = true;
     }
-    return console.log(controllo);
+    return controllo;
 }
 
-
 // Stampa l'array in console
-console.log(arrayVuoto);
+console.log(numeri);
